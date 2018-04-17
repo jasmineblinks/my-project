8b57e0ea175f20c4bfd11def1b2b3eeeea987354
jasmineblinks <puenehfaith@gmail.com>
2018-04-17 06:55:10 -0700

    My simple page design

---
+++ my figma.php.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>FAITH NG</title>
+        <style type="text/css">
+            
+
+ h2{
+    background-color: #ffffff
+    text-align:left;
+    font-family:Roboto;
+    font-style:regular;
+    font-size: 18px;
+    height: 24px;
+    width:83px;
+    display: inline-block;
+
+}
+ .h4{
+    background-color: #008000;
+    color: white;
+    font-family: roboto;
+    font-style: bold;
+    font-size: 14px;
+    line-height: 17px;
+    height: 10px;
+    width: 67px;
+    float: left;
+}
+h1{
+    background-color: #008000
+    color:#008000;
+    font-style:bold;
+    font-family: roboto;
+    line-height: 42px;
+    height: 35px;
+    width: 193px;
+}
+
+
+        </style>
+    </head>
+    <body>
+        <?php
+date_default_timezone_set("Africa/Lagos");
+echo date("h:i:s A");
+?>
+        <div>
+        <header style="background-color: #008000"><h2 align="left">FAITH NG</h2>
+        <h4 align="right">
+                     ABOUT
+                    CONTACT
+        </h4>
+    </header>
+        
+<img src="logo.png"  width="100%">
+        <div align="center"><h1><strong>WELCOME</strong></h1></div>
+        <p align="center">
+         Faith NG is a Nigeria Tech company that has the vision to build 5000 developers in Port harcourt,we also develop mobile apps(android and ios),web apps using(php,node.js,vue.js,bootstrap,react).
+        </p>
+        
+        </div>
+  <footer style="background-color: #008000"><h5 align="center">copyright 2018</h5></footer>
+    </body>
+</html>
